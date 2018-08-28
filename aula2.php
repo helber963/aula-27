cf80d45c0c15687f56710d34fc06d3cd022f0c50
Helber963 <helber963@gmail.com>
2018-08-27 21:24:41 -0300

    var-dump

--- aula2.php
+++ aula2.php
@@ -1,8 +1,11 @@
 <?php
-	class pessoa{
+	class Pessoa{
 		protected $altura;
 		protected $nome;
 
+		public function setName($nome) {
+			$this->nome = $nome;
+		}
 		public function andar (){
 			echo "Andando...";
 		}
--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 
-		require_once "Pessoa.php";
+		require_once "aula2.php";
+
 		$pessoa = new Pessoa;
+		
+		$pessoa->setNome("Helber Chaves");
+
 		$pessoa->andar();
+
+		var_dump($Pessoa);
+
+// Situação - 1
+//	(new Pessoa)->andar();
+// Situação - 2 
+//	new Pessoa;
 
 ?>
